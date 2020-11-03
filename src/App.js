668b06b11c2d05eb2fc1c076b6a1a6fdf371d12e
Your Name <hthanhlong97@gmail.com>
2020-11-03 11:55:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import background1 from "./assets/images/tour-1.jpg";
 import Title from "./common/Title";
 import ListImagesIcon from "./common/ListImagesIcon";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 function App() {
   const bigTitle = {
@@ -21,20 +22,20 @@ function App() {
     bgcolor: "rgba(22,105,134,0.7)",
   };
 
-  const titleTour = {
-    title: "#Chủ đề Tour",
-    bgcolor: "rgba(40,167,69,0.7)",
-  };
-
-  const titleHotel = {
-    title: "@ tên Hotel/Shop",
-    bgcolor: "rgba(0,0,0,0.7)",
-  };
-
-  const titleBestSeller = {
-    title: "Bán chạy nhất/ Siêu tiết kiệm",
-    bgcolor: "rgba(220,53,69,0.7)",
-  };
+  const supTitle = [
+    {
+      title: "#Chủ đề Tour",
+      bgcolor: "rgba(40,167,69,0.7)",
+    },
+    {
+      title: "@ tên Hotel/Shop",
+      bgcolor: "rgba(0,0,0,0.7)",
+    },
+    {
+      title: "Bán chạy nhất/ Siêu tiết kiệm",
+      bgcolor: "rgba(220,53,69,0.7)",
+    },
+  ];
 
   const classes = useStyles();
   return (
@@ -62,35 +63,40 @@ function App() {
                     left: 0,
                     right: 0,
                     color: "white",
-                    backgroundColor: "yellow",
+
                     padding: "1rem",
                     display: "flex",
                     justifyContent: "space-between",
                   }}
                 >
-                  <Box style={{ backgroundColor: "red" }}>
+                  <Box>
                     <Title
                       title={bigTitle.title}
                       bgcolor={bigTitle.bgcolor}
                       fontSize="1.2rem"
                     />
                     <Box style={{ marginLeft: "0.8rem", width: "auto" }}>
-                      <Title
-                        title={titleTour.title}
-                        bgcolor={titleTour.bgcolor}
-                      />
-                      <Title
-                        title={titleHotel.title}
-                        bgcolor={titleHotel.bgcolor}
-                      />
-                      <Title
-                        title={titleBestSeller.title}
-                        bgcolor={titleBestSeller.bgcolor}
-                      />
+                      {supTitle &&
+                        supTitle.map((item, index) => (
+                          <Title
+                            title={item.title}
+                            bgcolor={item.bgcolor}
+                            key={index}
+                          />
+                        ))}
                     </Box>
                   </Box>
-                  <Box style={{ backgroundColor: "green" }}>
-                    <Typography>Tra gop ne</Typography>
+                  <Box>
+                    <Box className={classes.Tragop}>
+                      <Typography variant="body2">Trả góp</Typography>
+                      <div className={classes.arrowRight}>
+                        <div>
+                          <FiberManualRecordIcon className={classes.icon} />
+                        </div>
+                      </div>
+                    </Box>
+
+                    <Typography>50% ne</Typography>
                   </Box>
                 </Box>
                 {/* Box ListImgIcon */}
@@ -113,37 +119,6 @@ function App() {
                   <ListImagesIcon urlImg="https://test-html.hahalolo.com/assets/img/icon/transportation.png" />
                   <ListImagesIcon urlImg="https://test-html.hahalolo.com/assets/img/icon/transportation.png" />
                 </Box>
-                <CardContent className={classes.content}>
-                  <Link style={{ textAlign: "left" }}>
-                    <Typography style={{ fontWeight: 700 }}>
-                      [Khuyến mãi 30%] Chỉ 179.000 đ cho Oriflame Nước Hoa Chính
-                      Hãng Thụy Sỹ
-                    </Typography>
-                  </Link>
-                  <div style={{ margin: "1rem 0" }}>
-                    <span style={{ display: "flex", textAlign: "left" }}>
-                      <Typography>Sẽ bắt đầu sau 6 giờ nữa.</Typography>
-                      <Typography style={{ margin: "0 0.2rem" }}>
-                        {" "}
-                        -{" "}
-                      </Typography>
-                      <Typography>Vẫn còn 2 phòng</Typography>
-                    </span>
-                    <span style={{ display: "flex" }}>
-                      <Typography>Bắt đầu: 22/05/2020</Typography>
-                      <Typography style={{ margin: "0 0.2rem" }}>
-                        {" "}
-                        -{" "}
-                      </Typography>
-                      <Typography>Kết thúc: 28/05/2020 </Typography>
-                    </span>
-                  </div>
-                  <div style={{ textAlign: "left" }}>
-                    <Button variant="contained" color="primary">
-                      Tìm hiểu thêm
-                    </Button>
-                  </div>
-                </CardContent>
               </Card>
             </Paper>
           </Grid>
@@ -169,6 +144,31 @@ const useStyles = makeStyles((theme) => ({
   },
   image: {
     height: "360px",
+  },
+
+  Tragop: {
+    position: "relative",
+    borderRadius: "0.1rem",
+    backgroundColor: "#E5782E",
+    padding: theme.spacing(0.21, 0.3, 0.21, 1),
+    marginRight: theme.spacing(2),
+  },
+  arrowRight: {
+    position: "absolute",
+    right: -15,
+    top: -0.5,
+    width: 0,
+    height: 0,
+    borderTop: "0.75rem solid transparent",
+    borderBottom: "0.75rem solid transparent",
+    borderLeft: "1rem solid #E5782E",
+    borderRadius: "0.1rem",
+  },
+  icon: {
+    position: "absolute",
+    fontSize: "0.5rem",
+    top: -4,
+    right: 8,
   },
 }));
 
