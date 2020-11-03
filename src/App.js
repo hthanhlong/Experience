ecba9b70aee7bdfa0fee8d43b3623e54e2adf759
Your Name <hthanhlong97@gmail.com>
2020-11-03 15:31:14 +0700

    update

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import {
   Paper,
   CardMedia,
   Card,
-  CardContent,
-  Link,
-  Button,
 } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import { makeStyles } from "@material-ui/core/styles";
 import background1 from "./assets/images/tour-1.jpg";
 import Title from "./common/Title";
@@ -17,6 +15,7 @@ import ListImagesIcon from "./common/ListImagesIcon";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CountReview from "./common/CountReview";
 import TitleReview from "./common/TitleReview";
+import Hidden from "@material-ui/core/Hidden";
 
 function App() {
   const bigTitle = {
@@ -82,114 +81,91 @@ function App() {
     },
   ];
 
-  const titleReview = {
-    title: "8.9",
-    bgcolor: "rgba(40,167,69,0.7)",
-  };
-
   const classes = useStyles();
 
   return (
     <Box component="div">
+      <CssBaseline />
       <header>This is la HEADER</header>
-      <Box component="main" className={classes.supContainer}>
-        <Grid container spacing={2}>
-          <Grid item xs={false} md={12} lg={3}>
+      <Grid
+        container
+        spacing={2}
+        component="main"
+        className={classes.supContainer}
+      >
+        <Hidden xsDown>
+          <Grid item md={12} lg={3}>
             <Paper className={classes.paper}>Column one</Paper>
           </Grid>
-          <Grid item xs={12} md={12} lg={6}>
-            <Paper className={classes.paper}>
-              <Card style={{ position: "relative" }}>
-                <CardMedia
-                  className={classes.image}
-                  component="img"
-                  src={background1}
-                  alt="tour-1"
-                />
-                <Box
-                  component="div"
-                  style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    color: "white",
-                    padding: "1rem",
-                    display: "flex",
-
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <Box>
-                    <Title
-                      title={bigTitle.title}
-                      bgcolor={bigTitle.bgcolor}
-                      fontSize="1.2rem"
-                    />
-                    <Box style={{ marginLeft: "0.8rem", width: "auto" }}>
-                      {supTitle &&
-                        supTitle.map((item, index) => (
-                          <Title
-                            title={item.title}
-                            bgcolor={item.bgcolor}
-                            key={index}
-                          />
-                        ))}
-                    </Box>
-                  </Box>
-                  <Box>
-                    <Box className={classes.Tragop}>
-                      <Typography variant="body2">Trả góp</Typography>
-                      <div className={classes.arrowRight}>
-                        <div>
-                          <FiberManualRecordIcon className={classes.icon} />
-                        </div>
-                      </div>
-                    </Box>
-                    <Typography>50% ne</Typography>
-                  </Box>
-                </Box>
-                <Box
-                  component="div"
-                  style={{
-                    position: "absolute",
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    color: "white",
-                    padding: "1rem",
-                    display: "flex",
-
-                    justifyContent: "space-between",
-                  }}
-                >
-                  {/* box listIcon*/}
-                  <Box display="flex" flexWrap="wrap" width="40%">
-                    {iconList &&
-                      iconList.map((i) => (
-                        <div>
-                          <ListImagesIcon key={i.id} {...i} />
-                        </div>
+        </Hidden>
+        <Grid item xs={12} md={12} lg={6}>
+          <Paper className={classes.paper}>
+            <Card style={{ position: "relative" }}>
+              <CardMedia
+                className={classes.image}
+                component="img"
+                src={background1}
+                alt="tour-1"
+              />
+              <Box component="div" className={classes.boxContainer}>
+                <Box>
+                  <Title
+                    title={bigTitle.title}
+                    bgcolor={bigTitle.bgcolor}
+                    fontSize="1.2rem"
+                  />
+                  <Box style={{ marginLeft: "0.8rem", width: "auto" }}>
+                    {supTitle &&
+                      supTitle.map((item, index) => (
+                        <Title
+                          title={item.title}
+                          bgcolor={item.bgcolor}
+                          key={index}
+                        />
                       ))}
                   </Box>
-                  {/* box review*/}
-                  <Box display="flex" height="100%">
-                    <TitleReview
-                      titleReviewUp="Xuất sắc"
-                      titleReviewDown="69 đánh giá"
-                    />
-                    <CountReview countReview="8.9" />
-                  </Box>
                 </Box>
-              </Card>
-              <Typography>Day la content</Typography>
-            </Paper>
-          </Grid>
+                <Box>
+                  <Box className={classes.Tragop}>
+                    <Typography variant="body2">Trả góp</Typography>
+                    <div className={classes.arrowRight}>
+                      <div>
+                        <FiberManualRecordIcon className={classes.icon} />
+                      </div>
+                    </div>
+                  </Box>
+                  <Typography>50% ne</Typography>
+                </Box>
+              </Box>
+              <Box component="div" className={classes.boxContainer2}>
+                {/* box listIcon*/}
+                <Box display="flex" flexWrap="wrap" width="40%">
+                  {iconList &&
+                    iconList.map((i) => (
+                      <div>
+                        <ListImagesIcon key={i.id} {...i} />
+                      </div>
+                    ))}
+                </Box>
+                {/* box review*/}
+                <Box display="flex" height="100%" mt="auto">
+                  <TitleReview
+                    titleReviewUp="Xuất sắc"
+                    titleReviewDown="69 đánh giá"
+                  />
+                  <CountReview countReview="8.9" />
+                </Box>
+              </Box>
+            </Card>
+            <Typography>Day la content</Typography>
+          </Paper>
+        </Grid>
+        <Hidden xsDown>
           <Grid item xs={false} md={12} lg={3}>
             <Paper className={classes.paper}>Column three</Paper>
           </Grid>
-        </Grid>
-      </Box>
+        </Hidden>
+      </Grid>
       <footer>This is Footer</footer>
     </Box>
   );
@@ -198,12 +174,10 @@ function App() {
 const useStyles = makeStyles((theme) => ({
   supContainer: {
     padding: "0 3rem",
-
     [theme.breakpoints.down("sm")]: {
       padding: 0,
     },
   },
-
   paper: {
     height: "500px",
     width: "100%",
@@ -240,6 +214,27 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "0.5rem",
     top: -4,
     right: 8,
+  },
+
+  boxContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    color: "white",
+    padding: "1rem",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  boxContainer2: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    color: "white",
+    padding: "1rem",
+    display: "flex",
+    justifyContent: "space-between",
   },
 }));
 
